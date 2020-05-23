0d76400bf1b5fec8b729a76dfd47151d78db3933
ethanarbuckle <ethan.a.arbuckle@gmail.com>
2020-05-23 10:30:10 -0700

    create vncserver daemon

---
+++ vncserverd/memory_limit.m
@@ -0,0 +1,10 @@
+
+#define MEMORYSTATUS_CMD_SET_JETSAM_TASK_LIMIT 6
+extern int memorystatus_control(int a, int b, int c, void *d, int e);
+
+static __attribute__((constructor)) void raise_memory_limit(void) {
+	NSLog(@"vncserverd bumping memory limit");
+	if (memorystatus_control(MEMORYSTATUS_CMD_SET_JETSAM_TASK_LIMIT, getpid(), 100, NULL, 0) == -1) {
+		NSLog(@"vncserverd failed to increase memory limit. Process will likely die");
+	}
+}
